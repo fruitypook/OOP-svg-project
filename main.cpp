bc55963f01055313c6bf17467e86a7c0d261d622
Aleks Y <aleksandar.k.yanchev@gmail.com>
2026-05-19 16:34:24 +0300

    first constructors

--- main.cpp
+++ main.cpp
@@ -1,12 +1,49 @@
+#include <cassert>
 #include <iostream>
 
+// {0,0} is top left corner
 struct Point {
-    int x, y;
+    unsigned x, y;
+};
+bool operator==(const Point& p1, const Point& p2) {
+    return (p1.x == p2.x && p1.y == p2.y);
+}
+bool operator!=(const Point& p1, const Point& p2) { return !(p1 == p2); }
+
+class Shape {
+   public:
+    Shape(const Point& p) : anchor(p) {}
+
+   protected:
+    Point anchor;
 };
 
-class Shape {};
-class Line : public Shape {};
-class Rectangle : public Shape {};
-class Circle : public Shape {};
+// anchor is other end of line
+class Line : public Shape {
+   public:
+    Line(const Point& p1, const Point& p2) : Shape(p1), end(p2) {
+        assert(p1 != p2);
+    }
 
-int main() { std::cout << "hai!"; }
+   private:
+    Point end;
+};
+
+// anchor is other end of diagonal
+class Rectangle : public Shape {
+   public:
+   private:
+    Point end;
+};
+
+// anchor is centre
+class Circle : public Shape {
+   public:
+   private:
+    unsigned radius;
+};
+
+int main() {
+    std::cout << "hai!";
+    //
+}
