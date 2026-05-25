74dc74cad4c12eab22e777ae558da076d60d8893
Aleks Y <aleksandar.k.yanchev@gmail.com>
2026-05-26 00:47:27 +0300

    small fix

--- svgfile.hpp
+++ svgfile.hpp
@@ -1,6 +1,6 @@
 #include "group.hpp"
 
-class SVGFile : public Group {
+class SVGFile : private Group {
    public:
     SVGFile(const char*);
     SVGFile(const SVGFile& o);
@@ -10,6 +10,7 @@ class SVGFile : public Group {
     // TODO: user commands here
     void serialize(std::ostream& os = std::cout, unsigned = 0) const override;
     void deserialize(std::istream& = std::cin) override;
+    using Group::addShape;  // making it public
 
     void print(const unsigned = 0, const unsigned = 0) const override;
 
